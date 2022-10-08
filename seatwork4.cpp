eceec2845cfd407110e2c9922faf185b16afddb0
Eagl3ye <vincejaphethloable@gmail.com>
2022-10-09 00:42:18 +0800

    final

--- seatwork4.cpp
+++ seatwork4.cpp
@@ -1,43 +1,61 @@
 #include <iostream>
 using namespace std;
 
-struct link
+struct Link
 {
     int value;
-    link *next = NULL;
+    Link *next = NULL;
 };
 
 int main(){
-    link *head = NULL;
-    link *anchor = NULL;
-    for(int i=1;i<=3;i++){
-        link newnode = new link();
+    //CONSTS
+    int num_nodes = 3;
+    int num_disp = 19;
+
+    //IMPLEMENTATION
+    Link *head = NULL;
+    Link *anchor = NULL;
+    for (int i = 1; i <= num_nodes; i++)
+    {
+        Link *newnode = new Link();
         if(head == NULL){
             head = newnode;
         }
         newnode->value = i;
-        if(anchor->nextnode == &head){
-            
-        }
-        if(newnode->nextnode == NULL{
-            newnode->nextnode = &head;
-        }
-        anchor = &newnode;
-    }
-    /*
-    link one, two, three;
-    one.value = 1;
-    one.next = &two;
-    two.value = 2;
-    two.next = &three;
-    three.value = 3;
-    three.next = &one;
+        
+        // DEBUG
+        //cout << newnode->value << " ";
+        // DEBUG
 
-    link *cursor = &one;
-    for(int i = 0; i<60; ++i){
-        cout<<cursor->value<<" ";
-        cursor = cursor->next;
+        if (newnode->next == NULL)
+        {
+            newnode->next = head;
+        }
+        if(anchor != NULL){
+            anchor->next = newnode;
+        }
+        anchor = newnode;
     }
-    */
+    
+    //DISPLAY
+    cout<<"\n\n\n"<<endl;
+    Link *cursor1 = head;
+    for (int i = 0; i < num_disp; ++i)
+    {
+        cout << cursor1->value <<" ["<<cursor1<< "]  -->  ";
+        cursor1 = (cursor1->next);
+    }
+
+    //DELETE
+    cout <<"\n\n\n"<<endl;
+    Link *cursor2 = head;
+    Link *temp;
+    for (int i = 1; i <= num_nodes; i++){
+        cout << "DELETING Node( " << cursor2 << " ) of value " << cursor2->value << endl;
+        temp = cursor2->next;
+        delete cursor2;
+        cursor2 = temp;
+    }
+
     return 0;
 }
